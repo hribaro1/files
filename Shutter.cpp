fb54cb34727cb3cb454d4544d996cb8e6db9cffe
hribaro1 <52284972+hribaro1@users.noreply.github.com>
2023-05-09 15:56:42 +0200

    Update Shutter.cpp

--- Shutter.cpp
+++ Shutter.cpp
@@ -1,10 +1,11 @@
 #include "Shutter.h"
 #include "Arduino.h"
 
-Shutter::Shutter(byte pinUp, byte pinDown) {
+Shutter::Shutter(byte pinUp, byte pinDown, int newPosition, int currentPosition) {
   _pinUp = pinUp;
   _pinDown = pinDown;
-
+  _newPosition = newPosition;
+  _currentPosition = currentPosition;
   init();
 }
 void Shutter::init() {
@@ -12,6 +13,29 @@ void Shutter::init() {
   pinMode(_pinDown, OUTPUT);
   stop();
 }
+
+
+void Shutter::setPosition(newPosition, currentPosition) {
+ if (newPosition >=  currentPosition) {   
+   rollTimeUp = calibratedTimeUp*(newPosition - currentPosition)/100;
+   rollStartTime= milldown();
+   if ((millis()-rollStartTime)<=rollTimeUp) {
+     up();
+   } else {
+     stop()
+   }
+       
+ } else {
+   rollTimeDown = calibratedTimeDown*(currentPosition - newPosition)/100;
+   rollStartTime= millis();
+   if ((millis()-rollStartTime)<=rollTimeDown) {
+     down();
+   } else {
+     stop();
+   }
+ }
+}
+
 void Shutter::up() {
   digitalWrite(_pinDown, LOW);
   delay(100);
